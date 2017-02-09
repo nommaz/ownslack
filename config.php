7f10d4a001a04517d731d500afca53944cd2caf2
Vimal <vimalpatel.ce@gmail.com>
2017-02-09 13:32:06 +0530

    channel, link and other parameter are set into

--- config.php
+++ config.php
@@ -14,7 +14,9 @@ DEFINE('DB_SERVER_PASSWORD', '');
 DEFINE('DB_DATABASE', 'owncloud');
 
 $conn = mysqli_connect(DB_SERVER, DB_SERVER_USERNAME, DB_SERVER_PASSWORD, DB_DATABASE) or die('Error connecting to database.');
-
+mysqli_set_charset($conn, "UTF8");
 // Create a constant to store your Slack URL
 define('SLACK_WEBHOOK', 'https://hooks.slack.com/services/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');
-define("DEFAULT_CHANNEL", "#random");
+define("DEFAULT_CHANNEL", "#general");
+define("BASE_URL", "https://docs.hugin.co/core/img/fileicons/");
+define("IMAGE_EXT", "png");
--- slack.php
+++ slack.php
@@ -5,18 +5,36 @@ include_once './config.php';
 $q = "SELECT * FROM `hg_patel` LIMIT 1";
 $res = mysqli_query($conn, $q);
 while ($row = mysqli_fetch_assoc($res)) {
-    sendMessage($row['name'] . " sending file at " . time());
+    $icon = BASE_URL . $row['mimetype'] . "." . IMAGE_EXT;
+    $msg = $row['type'];
+
+    $linkMimearr = array(4, 10, 12, 20, 100, 30, 31, 44, 42, 46, 55, 59, 60, 62, 76, 80, 81, 82, 85, 91, 99, 107, 108, 110);
+    if (in_array($row['mimetype'], $linkMimearr)) {
+        $link = "https://docs.hugin.co/index.php/apps/onlyoffice/" . $row['object_id'];
+    } else {
+        $link = "https://docs.hugin.co/index.php/apps/files/?fileid=" . $row['object_id'];
+    }
+
+    $size = formatBytes($row['size']);
+    $channel = '';
+    if ($row['type'] == 'file_created' || $row['type'] == 'file_changed') {
+        $channel = 'general'; // TODO : get firstname(folder name) from $row['path'] and then lookup the channel name from other table.. if not found then do not pass channel
+    }
+
+    sendMessage($msg, $icon, $channel);
 }
 
-function sendMessage($msg = '') {
+function sendMessage($msg = '', $icon = '', $channel = '') {
     // Make your message
     $data = array(
         'text' => $msg,
         "username" => "Vedant Test",
-        "icon_emoji" => ":ghost:",
-        "channel" => DEFAULT_CHANNEL,
+        "icon_emoji" => $icon,
         "link_names" => 1
     );
+    if (trim($channel) != '') {
+        $data["channel"] = $channel;
+    }
     $message = array('payload' => json_encode($data));
     // Use curl to send your message
     $c = curl_init(SLACK_WEBHOOK);
@@ -25,4 +43,11 @@ function sendMessage($msg = '') {
     curl_setopt($c, CURLOPT_POSTFIELDS, $message);
     curl_exec($c);
     curl_close($c);
+}
+
+function formatBytes($size, $precision = 2) {
+    $base = log($size, 1024);
+    $suffixes = array('', 'KB', 'MB', 'GB', 'TB');
+
+    return round(pow(1024, $base - floor($base)), $precision) . ' ' . $suffixes[floor($base)];
 }
